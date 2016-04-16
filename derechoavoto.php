5d499cb261ddde153e76d0b4bb76413b471614b6
avm99963 <jocdeladria@gmail.com>
2016-04-16 11:19:15 +0200

    Fixed Robots tag.

--- derechoavoto.php
+++ derechoavoto.php
@@ -14,7 +14,7 @@ include_once("votocore.php");
         <title>Verificar derecho a voto</title>
         <link rel="stylesheet" href="styles.css">
         <link rel="stylesheet" href="css/derechoavoto.css">
-        <meta name="robots" content="all">
+        <meta name="robots" content="none">
         <meta name="viewport" content="width=device-width, initial-scale=1">
     </head>
     <body>
